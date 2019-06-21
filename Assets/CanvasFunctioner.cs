7f05f13c1501ebb79aa9a3eb8c0266ac184360b3
Ozair Zaman <ozair.zaman@outlook.com>
2019-06-21 18:20:45 +1000

    fixed scene change code

---
+++ Assets/CanvasFunctioner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasFunctioner : MonoBehaviour
+{
+    public GameObject lightMcLightFace, mainCam, fpsCam;
+
+    public void ContinueWithCharacter()
+    {
+        //this.gameObject.SetActive(false);
+        //lightMcLightFace.SetActive(false);
+        //mainCam.SetActive(false);
+        //fpsCam.SetActive(true);
+
+        //go to next scene
+        SceneManager.LoadScene(1);
+    }
+
+    public void Update()
+    {
+        if (this.gameObject.activeSelf)
+        {
+            lightMcLightFace.SetActive(true);
+        }
+        else
+        {
+            lightMcLightFace.SetActive(false);
+        }
+    }
+}
---
+++ Assets/cb125.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cb125 : MonoBehaviour
+{
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("COLLISION");
+
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerQuest q = other.GetComponent<PlayerQuest>();
+            for (int i = 0; i < q.quests.Count; i++)
+            {
+                if (q.quests[i].name == "Big Trouble In Little China!")
+                {
+                    q.quests[i].goal.ObjectCollected();
+                    
+                }
+            }
+        }
+
+
+    }
+}
